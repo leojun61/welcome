ca39d04f929594d9f752b7c29859518e3bcb3028
leojun61 <63827689+leojun61@users.noreply.github.com>
2020-04-17 11:03:23 +0900

    test

---
+++ program.java
@@ -0,0 +1,16 @@
+package ex1.io;
+
+public class program {
+
+	public static void main(String[] args) {
+		
+		System.out.write('3'+0);
+		System.out.write('3'+1);
+		System.out.write('3'+2);
+		System.out.write('3'+3);
+		System.out.write('3'+4);
+		
+		System.out.flush();
+	}
+
+}
